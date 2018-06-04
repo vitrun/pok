11784880d0c09d5784dfb4e592c9b5a8e86ba3b3
vitrun <vitrun@gmail.com>
2018-06-05 00:22:25 +0800

    Added block

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,12 +1,12 @@
+import time
+
 
 class Block(object):
     """The block structure"""
-    def __init__(self):
-        self.index = None
-        self.timestamp = None
-        self.proof = None
-        self.prev_hash = None
-        self.transactions = []
+    def __init__(self, index, proof, prev_hash, transactions):
+        self.index = index
+        self.timestamp = time.time()
+        self.proof = proof
+        self.prev_hash = prev_hash
+        self.transactions = transactions
 
-    def new_block(self):
-        pass
--- blockchain/chain.py
+++ blockchain/chain.py
@@ -1,8 +1,13 @@
+from transaction import Transaction
+from block import Block
+
 
 class Chain(object):
     """The chain"""
     def __init__(self):
         self.blocks = []
+        self.current_transactions = []
+        self.new_block(1, 1)
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -12,4 +17,15 @@ class Chain(object):
         :param amount: <float> amount
         :return: <int> the containing block index
         """
-        pass
+        trx = Transaction(sender, recipient, amount)
+        self.current_transactions.append(trx)
+
+    def new_block(self, proof, previous_hash):
+        prev_index = self.last_block.index + 1 if self.last_block else 1
+        block = Block(prev_index, proof, previous_hash, self.current_transactions)
+        self.blocks.append(block)
+        return block
+
+    @property
+    def last_block(self):
+        return self.blocks[-1] if self.blocks else None
