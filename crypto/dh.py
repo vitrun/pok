b7afe466cf2bd27cf5f54065e7c3c17c7c22f0f5
vitrun <vitrun@gmail.com>
2018-07-13 10:19:25 +0800

    crypto format

--- crypto/dh.py
+++ crypto/dh.py
@@ -1,35 +1,34 @@
-import random
-import base64
+# encoding:utf8
 import hashlib
 import sys
 
-#: refer to https://asecuritysite.com/encryption/diffie_py?g=15&p=9887%20&a=13&b=15
-g=11
-p=1999
+#: refer to https://asecuritysite.com/encryption/diffie_py
+g = 11
+p = 1999
 
-a=int(sys.argv[1])
+a = int(sys.argv[1])
 
-b=int(sys.argv[2])
+b = int(sys.argv[2])
 
-A = (g**a) % p
-B = (g**b) % p
+A = (g ** a) % p
+B = (g ** b) % p
 
-print 'g: ',g,' (a shared value), n: ',p, ' (a prime number)'
+print('g: ', g, ' (a shared value), n: ', p, ' (a prime number)')
 
-print '\nAlice calculates:'
-print 'a (Alice random): ',a
-print 'Alice value (A): ',A,' (g^a) mod p'
+print('\nAlice calculates:')
+print('a (Alice random): ', a)
+print('Alice value (A): ', A, ' (g^a) mod p')
 
-print '\nBob calculates:'
-print 'b (Bob random): ',b
-print 'Bob value (B): ',B,' (g^b) mod p'
+print('\nBob calculates:')
+print('b (Bob random): ', b)
+print('Bob value (B): ', B, ' (g^b) mod p')
 
-print '\nAlice calculates:'
-keyA=(B**a) % p
-print 'Key: ',keyA,' (B^a) mod p'
-print 'Key: ',hashlib.sha256(str(keyA)).hexdigest()
+print('\nAlice calculates:')
+keyA = (B ** a) % p
+print('Key: ', keyA, ' (B^a) mod p')
+print('Key: ', hashlib.sha256(str(keyA)).hexdigest())
 
-print '\nBob calculates:'
-keyB=(A**b) % p
-print 'Key: ',keyB,' (A^b) mod p'
-print 'Key: ',hashlib.sha256(str(keyB)).hexdigest()
+print('\nBob calculates:')
+keyB = (A ** b) % p
+print('Key: ', keyB, ' (A^b) mod p')
+print('Key: ', hashlib.sha256(str(keyB)).hexdigest())
--- crypto/rsa.py
+++ crypto/rsa.py
@@ -1,30 +1,34 @@
+# encoding:utf8
 import sys
 
-#Let two primes be p = 7 and q = 13. Thus, modulus n = pq = 7 x 13 = 91.
+# Let two primes be p = 7 and q = 13. Thus, modulus n = pq = 7 x 13 = 91.
 p, q = 7, 13
 
-#Select e = 5, which is a valid choice since there is no number that is common factor of 5 and (p − 1)(q − 1) = 6 × 12 = 72, except for 1.
-print('select a valid e so that there is no number that is common factor of e and 72')
+# Select e = 5, which is a valid choice since there is no number that is common
+# factor of 5 and (p − 1)(q − 1) = 6 × 12 = 72, except for 1.
+print('select a valid e so that there is no number that is common factor of '
+      'e and 72')
 e, plain = int(sys.argv[1]), int(sys.argv[2])
 n = p * q
 
 if plain >= n:
     exit('number to be encrypted should not be greater than ' + str(n))
 
-#The pair of numbers (n, e) = (91, e) forms the public key and can be made available to anyone whom we wish to be able to send us encrypted messages.
+# The pair of numbers (n, e) = (91, e) forms the public key and can be made
+# available to anyone whom we wish to be able to send us encrypted messages.
 print('public key: ', (n, e))
 
-#Private Key d is the inverse of e modulo (p - 1)(q – 1): ed = 1 mod (p − 1)(q − 1)
-d, tmp = 0, (p-1)*(q-1)
+# Private Key d is the inverse of e modulo (p - 1)(q – 1):
+# ed = 1 mod (p − 1)(q − 1)
+d, tmp = 0, (p - 1) * (q - 1)
 for i in range(1, tmp):
     if e * i % tmp == 1:
-        d = i
+      d = i
 
 print('private key: ', (n, d))
 
-encrypted = plain**e % n
+encrypted = plain ** e % n
 
 print('encrypted: ', encrypted)
 
-print('decrypted: ', encrypted**d % n)
-
+print('decrypted: ', encrypted ** d % n)
